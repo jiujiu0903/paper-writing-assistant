9ea7f6d68203a0792c50c7d8819618ebb1ef5cd4
jiujiu0903 <452963338@qq.com>
2026-07-27 16:43:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ const ASSETS = [
   "./app.js",
   "./config.js",
   "./manifest.webmanifest",
-  "./icon-192.svg",
-  "./icon-512.svg"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
